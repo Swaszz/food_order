fbb2732ded1f89af4a881187914b69bc470505bc
Swaszz <swathykrishna2227@gmail.com>
2025-02-21 17:41:08 +0530

    files

--- src/server.js
+++ src/server.js
@@ -10,30 +10,29 @@ const apiRouter = require('./routes');
 const dbpassword = process.env.DB_Password
 console.log(dbpassword)
 mongoose.connect(`mongodb+srv://swathykrishna2227:${dbpassword}@cluster0.8vauz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-.then(res => {
-  console.log("DB CONNECTED")
-})
-.catch(err => {
-  console.error("DB FAILED:", err.message)  
-})
+  .then(res => {
+    console.log("DB CONNECTED")
+  })
+  .catch(err => {
+    console.error("DB FAILED:", err.message)
+  })
 
 const PORT = process.env.PORT || 3000;
-app.get("/api/test",(req,res)=>{
-  res.json ({message:"API is working"});
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working" });
 });
 app.use(
   cors({
-      origin: ["http://localhost:5173","https://frontfood-for.vercel.app"],
-      methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-      credentials: true,
+    origin: ["http://localhost:5173", "https://frontfood-for.vercel.app", "https://food-order-cnsc.vercel.app"],
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
   })
 );
 
 app.use(cookieParser())
 app.use(express.json())
-app.use('/api',apiRouter)
+app.use('/api', apiRouter)
 
 app.listen(3000, () => {
   console.log("server Running on http://localhost:3000")
 })
-      
